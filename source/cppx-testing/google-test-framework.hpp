2852f3895cc642e89fea17676ce170125acd0b5f
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-11-22 16:44:35 +0100

    Initial commit. Extracted from the cppx-core library.

---
+++ source/cppx-testing/google-test-framework.hpp
@@ -0,0 +1,17 @@
+﻿// Aborted work. Google test does not support hierarchy.
+
+#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
+#include <gtest/gtest.h>
+
+#include <cppx-core-language/meta-macro/macro-apply.hpp>
+#include <cppx-core-language/meta-macro/macro-duplicate.hpp>
+
+namespace gtest = testing;
+
+#ifndef CPPX_NO_DOLLARS_PLEASE
+#   define $expect              EXPECT_TRUE
+#   define $expect_eq           EXPECT_EQ
+#   define $require             ASSERT_TRUE
+#   define $require_eq          ASSERT_EQ
+#   define $test_case           TEST
+#endif
